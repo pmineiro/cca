8b483d85a9b293df61d8137f8d154c15036ad431
Paul Mineiro <paul.mineiro@gmail.com>
2014-11-11 11:43:57 -0800

    faster neighbors

--- ngrams/neighbors.py
+++ ngrams/neighbors.py
@@ -1,17 +1,5 @@
 from sys import argv,stdin
-from bisect import bisect
 import numpy as np, h5py
-
-def norm(x):
-    return np.inner(x,x)
-
-def dist(x,y):
-    return norm(x-y)
-
-def nn(q,d,v):
-    dists=[(dist(v[:,d[w]],q),w) for w in d]
-    dists.sort()
-    return dists[:5]
 
 f=h5py.File(argv[1],'r');
 data=f.get('megaproj');
@@ -19,10 +7,12 @@ data=np.array(data);
 words=open(argv[2],'r');
 k=int(argv[3])
 eigendict=dict()
+reverseeigendict=list()
 for line in words:
     parts=line.strip().split()
     word=parts[1]
     eigendict[word]=int(parts[0])-1
+    reverseeigendict.append(word)
 f.close()
 
 vocab=eigendict.keys()
@@ -43,11 +33,12 @@ def main():
             print "%s not found"%w3
             continue
         query=data[:,eigendict[w3]]+data[:,eigendict[w2]]-data[:,eigendict[w1]];
+        scores=sorted(zip(query.T.dot(data),range(len(eigendict))),reverse=True);
         if w1 == w2 and w2 == w3:
             print 'nearest neighbors of %s are ...'%(w1)
         else:
             print '%s is to %s as %s is to ...'%(w1,w2,w3)
-        print nn(query,eigendict,data)
+        print [(x,reverseeigendict[y]) for (x,y) in scores[:5]]
         line=raw_input()
 
 if __name__=='__main__':
